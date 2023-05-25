57f7f5948a7e072769856158a09ce80b6e67927f
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-05-26 00:17:29 +0545

    Feat:Todo UI done

---
+++ src/components/todoform/todoform.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+
+const Todoform = () => {
+  return (
+    <div className="todo-container width-100">
+      <div className="todo-container width-50">
+        <img src="./images/Todo.svg" alt="todo"></img>
+        <div className="inputclass">
+          <input
+            type="text"
+            name="text"
+            id="text"
+            placeholder="Add items/task here"
+          />
+          <button className="add-btn">Add</button>
+        </div>
+        <div className="ul">
+          <div className="li">
+            <h6 className="crossText">Motivation to code</h6>
+            <div className="action">
+              <button className="completed">completed</button>
+              <button className="delete">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todoform;
