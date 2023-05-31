2dd6a322b547b2d281913cf3cd9371e0ec31f828
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-05-31 23:09:55 +0545

    Make add button functional

--- src/components/todoform/todoform.jsx
+++ src/components/todoform/todoform.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 const Todoform = () => {
+  const [inputTask,setInputTask]=useState("");
+  const [itemsList,setItemsList]=useState([]);
+
+  // adding task/items functions
+  const addTask=()=>{
+    if(!inputTask){
+      alert(`Please fill up the task/items`);
+    }
+    else{
+      const myNewInputData={
+        id:new Date().getTime().toString(),
+        name:inputTask
+      }
+      setItemsList([...itemsList,myNewInputData]);
+      setInputTask('');
+    }
+  }
   return (
     <div className="todo-container width-100">
       <div className="todo-container width-50">
@@ -12,18 +29,26 @@ const Todoform = () => {
             name="text"
             id="text"
             placeholder="Add items/task here"
+            value={inputTask}
+            onChange={(event)=>setInputTask(event.target.value)}
           />
-          <button className="add-btn">Add</button>
+          <button className="add-btn" onClick={addTask}>Add</button>
         </div>
-        <div className="ul">
-          <div className="li">
-            <h6 className="crossText">Motivation to code</h6>
-            <div className="action">
-              <button className="edit">Edit</button>
-              <button className="completed">completed</button>
-              <button className="delete">Delete</button>
-            </div>
-          </div>
+        <div className="showitems">
+          {
+            itemsList.map((curElem,index)=>{
+              return(
+                <div className="li">
+                <h6 className="crossText">{curElem.name}</h6>
+                <div className="action">
+                  <button className="edit">Edit</button>
+                  <button className="completed">completed</button>
+                  <button className="delete">Delete</button>
+                </div>
+              </div>
+              )
+            })
+          }
         </div>
       </div>
     </div>
