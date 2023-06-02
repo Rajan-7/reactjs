06335dd5fc3a08043efa45909623402c88852fc7
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-06-02 18:58:26 +0545

    Make edit button functional

--- src/components/todoform/todoform.jsx
+++ src/components/todoform/todoform.jsx
@@ -1,30 +1,53 @@
 import React, { useState } from "react";
 import "./style.css";
 
+
 const Todoform = () => {
   const [inputTask, setInputTask] = useState("");
   const [itemsList, setItemsList] = useState([]);
+  const [editedTaskList, setEditedTaskList] = useState("");
 
   // adding task/items functions
   const addTask = () => {
     if (!inputTask) {
       alert(`Please fill up the task/items`);
+    } else if (editedTaskList) {
+      setItemsList(
+        itemsList.map((curElem) => {
+          if (curElem.id === editedTaskList) {
+            return { ...curElem, name: inputTask };
+          }
+          return curElem;
+        })
+      );
+      setInputTask("");
+      setEditedTaskList("");
     } else {
       const myNewInputData = {
         id: new Date().getTime().toString(),
         name: inputTask,
       };
-      setItemsList( [...itemsList,myNewInputData]);
+      setItemsList([...itemsList, myNewInputData]);
       setInputTask("");
     }
   };
+
   // delete items/task functions
-  const deleteItems=(index)=>{
-    const updatedNewItems=itemsList.filter((curElem)=>{
+  const deleteItems = (index) => {
+    const updatedNewItems = itemsList.filter((curElem) => {
       return curElem.id !== index;
-    })
+    });
     setItemsList(updatedNewItems);
-  }
+  };
+
+  // editing items/task functions
+  const edititems = (index) => {
+    const myEditedItems = itemsList.find((curElem) => {
+      return curElem.id === index;
+    });
+    setInputTask(myEditedItems.name);
+    setEditedTaskList(index);
+  };
 
   return (
     <div className="todo-container width-100">
@@ -49,7 +72,14 @@ const Todoform = () => {
               <div className="li">
                 <h6 className="crossText">{curElem.name}</h6>
                 <div className="action">
-                  <button className="edit">Edit</button>
+                  <button
+                    className="edit"
+                    onClick={() => {
+                      edititems(curElem.id);
+                    }}
+                  >
+                    Edit
+                  </button>
                   <button className="completed">completed</button>
                   <button
                     className="delete"
